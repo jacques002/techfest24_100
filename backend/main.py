11565bd510fd7a5351aa6d375ee9aa5c23e48b10
FlyingCoconut1 <92644292+FlyingCoconut1@users.noreply.github.com>
2024-02-04 19:41:13 +0800

    Register and Login P2

--- backend/main.py
+++ backend/main.py
@@ -1,17 +1,21 @@
 from fastapi import FastAPI
 import os 
 from dotenv import load_dotenv
-from src.routers.explain_router import explain_router
 load_dotenv()
 
+from src.routers.explain_router import explain_router
+from src.routers.auth_routers import auth_router
+
+environment = os.getenv("ENVIRONMENT")
 os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
 
 app = FastAPI()
 app.include_router(explain_router)
-
+app.include_router(auth_router)
 
 @app.get(
         path="/",
         tags=["root"])
+
 async def root():
     return {"message": "Hello Techfest24!"}
